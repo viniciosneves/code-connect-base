d759a40f41ee6f52d63631b62675d7d16e7296f1
Vinicios Neves <vinicios_neves@hotmail.com>
2024-01-17 20:20:37 +0000

    video 5.1

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -2,23 +2,28 @@ import Image from "next/image"
 import { Avatar } from "../Avatar"
 
 import styles from './cardpost.module.css'
+import Link from "next/link"
 
 export const CardPost = ({ post }) => {
-    return (<article className={styles.card}>
-        <header className={styles.header}>
-            <figure>
-                <Image src={post.cover} width={438} height={192} alt="Capa do artigo"/>
-            </figure>
-        </header>
-        <section className={styles.body}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-            <footer className={styles.footer}>
-                <Avatar 
-                    imageSrc={post.author.avatar} 
-                    name={post.author.username}
-                />
-            </footer>
-        </section>
-    </article>)
+    return (
+        <article className={styles.card}>
+            <Link href={`/posts/${post.slug}`}>
+                <header className={styles.header}>
+                    <figure>
+                        <Image src={post.cover} width={438} height={192} alt="Capa do artigo" />
+                    </figure>
+                </header>
+                <section className={styles.body}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                    <footer className={styles.footer}>
+                        <Avatar
+                            imageSrc={post.author.avatar}
+                            name={post.author.username}
+                        />
+                    </footer>
+                </section>
+            </Link>
+        </article>
+    )
 }
