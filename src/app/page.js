f8b514071c7e9dfc21721474be2964ace7532ec0
Vinicios Neves <vinicios_neves@hotmail.com>
2024-01-17 19:38:13 +0000

    video 4.2

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import { CardPost } from "@/components/CardPost"
 
 import styles from './page.module.css'
 import { logger } from "@/logger"
+import Link from "next/link"
 
 async function getAllPosts(page = 1) {
   const res = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
@@ -18,13 +19,20 @@ async function getAllPosts(page = 1) {
  
 
 export default async function Home() {
-  const { data: posts } = await getAllPosts()
+
+  const { data: posts, next, prev } = await getAllPosts()
 
   return (
     <main>
       <div className={styles.main}>
         {posts.map(post => <CardPost key={post.id} post={post} />)}
       </div>
+      {prev && <Link href={`?page=${prev}`}>
+        próxima anterior
+      </Link>}
+      {next && <Link href={`?page=${next}`}>
+        próxima página
+      </Link>}
     </main>
   )
 }
