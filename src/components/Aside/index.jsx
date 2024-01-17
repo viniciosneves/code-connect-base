72eb7a1f329b8f5c0b98eff150d119b573753bf2
Vinicios Neves <vinicios_neves@hotmail.com>
2024-01-17 11:57:05 +0000

    video 1.4

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,7 +1,11 @@
 import styles from './aside.module.css'
+import Image from 'next/image'
+
+import logo from './logo.png'
 
 export const Aside = () => {
     return (<aside className={styles.aside}>
-        <img src="/logo.png" alt="" />
+        <Image src={logo} alt="" />
+        {/* <img src="/logo.png" alt="" /> */}
     </aside>)
 }
